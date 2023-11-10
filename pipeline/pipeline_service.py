371ad98b9c91ce88bd51f4438b950002f064391b
HM <hieumdd@gmail.com>
2023-11-10 19:11:56 +0700

    feat: Add tables

--- pipeline/pipeline_service.py
+++ pipeline/pipeline_service.py
@@ -4,7 +4,7 @@ from compose import compose
 
 from db.bigquery import load
 from db.mysql import get
-from pipeline.pipeline_const import SalesCall3, ZNS
+from pipeline.pipeline_const import SalesCall3, ZNS, ZNS_FOLLOW, ZNS_FOLLOW_STORE
 
 
 def pipeline_service() -> dict[str, Union[str, int]]:
@@ -13,7 +13,7 @@ def pipeline_service() -> dict[str, Union[str, int]]:
             lambda rows: load(rows, pipeline.name, pipeline.schema),
             lambda: [pipeline.transform(row) for row in get(pipeline.query)],
         )()
-        for pipeline in [SalesCall3, ZNS]
+        for pipeline in [SalesCall3, ZNS, ZNS_FOLLOW, ZNS_FOLLOW_STORE]
     ]
 
     return {"results": results}
